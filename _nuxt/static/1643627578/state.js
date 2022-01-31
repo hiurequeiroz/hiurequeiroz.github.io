521ded2da9dc2c974eae52fdd1356c3817d0bfde
hiurequeiroz <hiure@riseup.net>
2022-01-31 08:13:50 -0300

    fix of the fix layout and images

---
+++ _nuxt/static/1643627578/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643627578",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"756d6674"}}}}(null,"\u002F"));
