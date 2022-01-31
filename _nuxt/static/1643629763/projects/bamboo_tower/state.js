f69e02df26ad9f957bf43ac04d989f753b99267d
hiurequeiroz <hiure@riseup.net>
2022-01-31 08:50:18 -0300

    add .nojekyll file to fix crache image

---
+++ _nuxt/static/1643629763/projects/bamboo_tower/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643629763",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fbamboo_tower",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3246f989"}}}}(null));
