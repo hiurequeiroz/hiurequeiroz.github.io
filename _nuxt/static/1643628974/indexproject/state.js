0d6694d732fd00948d72467aa2e6a95f7087ef7c
hiurequeiroz <hiure@riseup.net>
2022-01-31 08:37:42 -0300

    fix to fix to fix.... 2

---
+++ _nuxt/static/1643628974/indexproject/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643628974",layout:"pages",error:a,serverRendered:true,routePath:"\u002Findexproject",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0d21d173"}}}}(null));
