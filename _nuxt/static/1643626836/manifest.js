5488a5b3c722534765ccfabdbaa8d7389824586e
hiurequeiroz <hiure@riseup.net>
2022-01-31 08:01:30 -0300

    fix images and layout

---
+++ _nuxt/static/1643626836/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontato","\u002Fsobre","\u002Findexproject","\u002F","\u002Fblog","\u002Fposts\u002Fjellyfin","\u002Fprojects\u002Fbamboo_tower"]})
---
+++ _nuxt/static/1643626836/sobre/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643626836",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fsobre",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"106fa59e"}}}}(null));
