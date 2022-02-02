51f84872111bb1880bb0c8f4856f2c46b23c0ae8
hiurequeiroz <hiure@riseup.net>
2022-02-02 09:42:16 -0300

    add responsive design to videos

---
+++ _nuxt/static/1643805700/posts/Arduino/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643805700",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FArduino",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6b34e1d3"}}}}(null));
