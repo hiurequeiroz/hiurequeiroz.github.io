9bfb34dcd1f25f8c1ff23cae63f95d3ca88e4234
hiurequeiroz <hiure@riseup.net>
2022-02-01 21:06:20 -0300

    add preview again

---
+++ _nuxt/static/1643760261/posts/Motores/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643760261",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FMotores",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b4963b67"}}}}(null));
