0bf567f918fa65d2daf145f87b7c7c14ef849127
hiurequeiroz <hiure@riseup.net>
2022-02-01 21:49:28 -0300

    fix author

---
+++ _nuxt/static/1643762945/posts/Utilizando Ubus no LiMe-LROs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643762945",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FUtilizando%20Ubus%20no%20LiMe-LROs",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"618b136d"}}}}(null));
