d332e4d54ba1e7cf31c9c85fe0a5ecabd18f4517
hiurequeiroz <hiure@riseup.net>
2022-02-02 16:24:34 -0300

    add video psp and fit projetct page

---
+++ _nuxt/static/1643829672/posts/Interface de Monitoramento/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643829672",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FInterface%20de%20Monitoramento",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c08fb61f"}}}}(null));
