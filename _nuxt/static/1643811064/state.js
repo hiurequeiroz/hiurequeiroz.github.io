d7b598825dd1cc38921a45c924d215c7e47e9001
hiurequeiroz <hiure@riseup.net>
2022-02-02 11:11:26 -0300

    twitter componet in index page

---
+++ _nuxt/static/1643811064/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643811064",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b6ffa0fe"}}}}(null,"\u002F"));
