d28ed5ce9d545b9fcea7de8eabf40eef57e03d2e
hiurequeiroz <hiure@riseup.net>
2022-02-02 19:36:22 -0300

    amenizando uma cagada misteriosa

---
+++ _nuxt/static/1643840729/projects/Plastico/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643840729",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fprojects\u002FPlastico",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"91e3a43d"}}}}(null));
