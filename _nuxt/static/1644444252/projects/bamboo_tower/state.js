26cb3f0003645ffabf35bc6ca4c596995a44c313
hiurequeiroz <hiure@riseup.net>
2022-02-09 19:05:05 -0300

    arruma player do youtube

---
+++ _nuxt/static/1644444252/projects/bamboo_tower/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644444252",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fbamboo_tower",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8538cd8a"}}}}(null));
---
+++ _nuxt/static/1644444252/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644444252",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8538cd8a"}}}}(null,"\u002F"));
