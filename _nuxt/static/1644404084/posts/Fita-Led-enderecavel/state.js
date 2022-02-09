e59f185baac15031013449c6caaf3b48e9c22abd
hiurequeiroz <hiure@riseup.net>
2022-02-09 07:55:01 -0300

    centraliza imagens

---
+++ _nuxt/static/1644404084/posts/Fita-Led-enderecavel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644404084",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FFita-Led-enderecavel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"735d3b3a"}}}}(null));
