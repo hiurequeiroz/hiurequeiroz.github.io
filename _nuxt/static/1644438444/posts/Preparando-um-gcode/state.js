de1ef080bd7970e9d7f9171e30775b470f63a426
hiurequeiroz <hiure@riseup.net>
2022-02-09 17:27:57 -0300

    arruma alinhamentos e centralização de posts e videos

---
+++ _nuxt/static/1644438444/posts/Preparando-um-gcode/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644438444",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FPreparando-um-gcode",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b89a1505"}}}}(null));
