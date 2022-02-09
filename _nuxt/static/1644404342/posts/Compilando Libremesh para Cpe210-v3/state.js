0a4c3637615405332298f9203d2db21da12f479c
hiurequeiroz <hiure@riseup.net>
2022-02-09 08:00:13 -0300

    traduz pequenas coisas

---
+++ _nuxt/static/1644404342/posts/Compilando Libremesh para Cpe210-v3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644404342",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FCompilando%20Libremesh%20para%20Cpe210-v3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"20bc13ff"}}}}(null));
