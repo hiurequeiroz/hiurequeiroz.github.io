aca4c0268199f6b85d1b5376a6976db1b898e1fd
hiurequeiroz <hiure@riseup.net>
2022-02-09 07:43:45 -0300

    sobre em ingles

---
+++ _nuxt/static/1644403370/posts/Calibrando-a-temperatura-do-filamento/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644403370",layout:"pages",error:a,serverRendered:true,routePath:"\u002Fposts\u002FCalibrando-a-temperatura-do-filamento",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2c8d2331"}}}}(null));
